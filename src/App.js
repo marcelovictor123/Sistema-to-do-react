3cbfde078b289f145cb5e7460b63a67b18d0062a
marcelo_victor <marcelo.victor525@gmail.com>
2023-03-02 21:13:46 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import { BsTrash, BsBookmarkChek, BsBookmarkChekFill } from "react-icons/bs";
+
+const API = "http://localhost:5000";
 
 function App() {
+  const [title, setTitle] = useState("")
+  const [time, setTime] = useState("")
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Enviou")
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-header">
+        <h1>React Todo</h1>
+      </div>
+      <div className="form-todo">
+        <h2>Insira sua proxima tarefa</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="submit" value="Enviar"></input>
+        </form>
+      </div>
+      <div className="list-todo">
+        <h2>Lista de tarefas</h2>
+        {todos.length === 0 && 'Não ha tarefas'}
+      </div>
     </div>
   );
 }
